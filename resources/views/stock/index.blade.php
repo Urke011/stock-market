602881628dc11224cc8411ee444d037e75f42208
Uros Rajkovic <urosrajkovic011@hotmail.com>
2024-05-05 23:42:49 +0200

    modify main page

--- resources/views/stock/index.blade.php
+++ resources/views/stock/index.blade.php
@@ -17,25 +17,30 @@
         <h1>Easily invest with us in crypto and stock market shares</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, saepe!</p>
         <div>
+            <a href="">
             <button type="button" class="btn btn-primary m-2">Contact us!</button>
-
+            </a>
+            <a href="#main-stock-wrapper">
             <button type="button" class="btn btn-primary m-2">Portfolio</button>
+            </a>
         </div>
     </div>
 </div>
 <!-- main part -->
-<div class="main-stock-wrapper bg-light mt-4 p-2" style="overflow: hidden;">
+<div class="main-stock-wrapper bg-light mt-4 p-2" id="main-stock-wrapper" style="overflow: hidden;">
     <div class="row">
         <div class="col-2">
-            <div>
-                <div class="bg-dark">
-                    <p class="text-white">Logo</p>
+            <div class="bg-dark">
+                <div class="bg-dark p-3">
+                    <p class="text-white mb-5">Logo</p>
                     <p class="text-white">Cene ulaganja</p>
                 </div>
-                <p>Home</p>
-                <p>Blog, News</p>
-                <p>Setings</p>
-                <p>Forum</p>
+                <div class="p-3 mt-5" style="background-color: #014c82;">
+                <p class="mb-5 text-white">Home</p>
+                <p class="mb-5 text-white">Blog, News</p>
+                <p class="mb-5 text-white">Setings</p>
+                <p class="mb-5 text-white">Forum</p>
+                </div>
             </div>
         </div>
         <div class="col-10">
@@ -49,54 +54,93 @@
                 </form>
             </div>
             <div class="d-flex mt-3">
-                <div class="border col">
-                    <p><img src="{{'assets/images/logo/Microsoft_logo_(2012).svg'}}" alt="logo"
+                <div class="border col p-2">
+                    <p class="text-end"><img src="{{'assets/images/logo/Microsoft_logo_(2012).svg'}}" alt="logo"
                             style="width: 4rem;"></p>
-                    <p>Name</p>
-                    <p>Price</p>
-                    <p>Score</p>
+                    <p class="fw-bold">Name</p>
+                    <p class="fw-bold">Price</p>
+                    <p class="fw-bold">Score</p>
                 </div>
-                <div class="border col">
-                    <p><img src="{{'assets/images/logo/Microsoft_logo_(2012).svg'}}" alt="logo"
+                <div class="border col p-2">
+                    <p class="text-end"><img src="{{'assets/images/logo/Microsoft_logo_(2012).svg'}}" alt="logo"
                             style="width: 4rem;"></p>
-                    <p>Name</p>
-                    <p>Price</p>
-                    <p>Score</p>
+                    <p class="fw-bold">Name</p>
+                    <p class="fw-bold">Price</p>
+                    <p class="fw-bold">Score</p>
                 </div>
-                <div class="border col">
-                    <p><img src="{{'assets/images/logo/Microsoft_logo_(2012).svg'}}" alt="logo"
+                <div class="border col p-2">
+                    <p class="text-end"><img src="{{'assets/images/logo/Microsoft_logo_(2012).svg'}}" alt="logo"
                             style="width: 4rem;"></p>
-                    <p>Name</p>
-                    <p>Price</p>
-                    <p>Score</p>
+                    <p class="fw-bold">Name</p>
+                    <p class="fw-bold">Price</p>
+                    <p class="fw-bold">Score</p>
                 </div>
-                <div class="border col">
-                    <p><img src="{{'assets/images/logo/Microsoft_logo_(2012).svg'}}" alt="logo"
+                <div class="border col p-2">
+                    <p class="text-end"><img src="{{'assets/images/logo/Microsoft_logo_(2012).svg'}}" alt="logo"
                             style="width: 4rem;"></p>
-                    <p>Name</p>
-                    <p>Price</p>
-                    <p>Score</p>
+                    <p class="fw-bold">Name</p>
+                    <p class="fw-bold">Price</p>
+                    <p class="fw-bold">Score</p>
                 </div>
-                <div class="border col">
-                    <p><img src="{{'assets/images/logo/Microsoft_logo_(2012).svg'}}" alt="logo"
+                <div class="border col p-2">
+                    <p class="text-end"><img src="{{'assets/images/logo/Microsoft_logo_(2012).svg'}}" alt="logo"
                             style="width: 4rem;"></p>
-                    <p>Name</p>
-                    <p>Price</p>
-                    <p>Score</p>
+                    <p class="fw-bold">Name</p>
+                    <p class="fw-bold">Price</p>
+                    <p class="fw-bold">Score</p>
                 </div>
             </div>
-            <div class="d-flex">
+            <div class="d-flex justify-content-around m-5">
             <div class="line-chart">
-                <p><img src="{{'assets/images/logo/rtaImage.png'}}" alt=""></p>
+                <p><img src="{{'assets/images/logo/rtaImage.png'}}" alt="" style="width: 100%;"></p>
             </div>
-                <div>
-                    <p>Lista zelja</p>
-                    
+                <div class="border rounded p-5 m-2 text-center" style="background-color: #7abd30;">
+                    <p class="text-white fs-3">Lista zelja</p>
+                    <div class="d-flex">
+                        <div>
+                        <ol>
+                            <li class="text-white fs-4">Microsoft 120$</li>
+                            <li class="text-white fs-4">Microsoft 120$</li>
+                            <li class="text-white fs-4">Microsoft 120$</li>
+                            <li class="text-white fs-4">Microsoft 120$</li>
+                            <li class="text-white fs-4">Microsoft 120$</li>
+                            <li class="text-white fs-4">Microsoft 120$</li>
+                            <li class="text-white fs-4">Microsoft 120$</li>
+                        </ol>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-
+    <div class="slider-part">
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active" style="height: 50vh;">
+                    <img src="{{'assets/images/sliders/blogging-guide-K5DY18hy5JQ-unsplash.jpg'}}" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item" style="height: 50vh;">
+                    <img src="{{'assets/images/sliders/nick-chong-N__BnvQ_w18-unsplash.jpg'}}" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item" style="height: 50vh;">
+                    <img src="{{'assets/images/sliders/blogging-guide-K5DY18hy5JQ-unsplash.jpg'}}" class="d-block w-100" alt="...">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
 </div>
 <div>
     @if(session()->has('success'))
